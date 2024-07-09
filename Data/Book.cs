58868c701834e88a1964b3b00623eba5082429bf
MNU <mathieu.nussbaumer@brisoft.ch>
2024-07-09 14:16:13 +0200

    Add some books

---
+++ Data/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace BooksLibrary.Data
+{
+    public class Book
+    {
+        public int ID { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Publisher { get; set; }
+
+        public int? Year { get; set; }
+
+        public string? Subject { get; set; }
+    }
+}
---
+++ Services/BooksService.cs
@@ -0,0 +1,25 @@
+﻿using BooksLibrary.Data;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+using static System.Net.WebRequestMethods;
+
+namespace BooksLibrary.Services
+{
+    public class BooksService
+    {
+        private List<Book>? books;
+
+        private HttpClient http { get; set; }
+
+        public BooksService(HttpClient httpClient) 
+        {
+            http = httpClient;
+        }
+
+        public async Task<List<Book>> GetBooks()
+        {
+            var result = await http.GetFromJsonAsync<List<Book>>("sample-data/books.json");
+            return result;
+        }
+    }
+}
